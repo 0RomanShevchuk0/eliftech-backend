ee62b3cab6c651561bc81545890c5b59b05af6e3
RomanShevchuk0 <roman.shevchuk@dmorpheus.design>
2025-03-19 10:34:56 +0100

    filters

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ExceptionsFilter } from './filters/exception.filter';
+import { PrismaExceptionFilter } from './filters/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  app.useGlobalFilters(new ExceptionsFilter(), new PrismaExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
